29f7dc25e078212779414c4167e5ff6b16cb6733
Roberto <roberto@maxmare.com>
2017-01-20 14:51:19 -0800

    Update transfer.cpp

--- transfer.cpp
+++ transfer.cpp
@@ -10,7 +10,19 @@ pListBuffer shape_hexagon_create(real32 radius){
 	void* pList;
 	
 	
-	pListBuffer buf
+	pListBuffer buf;
 	
 	return pList;	
 }
+
+/*
+:: python backup tool,
+
+:: read drive argument to know the path we are using,
+:: scan the entire drive collecting names, modification dates, types and sizes of all files and folders.
+:: store all the info in a graph
+:: read the previous graph and compare it recursively to the current,
+:: for each item,
+	:: if current item does not exists in prev graph.
+//*/
+
