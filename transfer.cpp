1d96f2f848a10400b937105645d714c2ea19fd73
Roberto <roberto@maxmare.com>
2017-03-10 16:41:46 -0800

    Update transfer.cpp

--- transfer.cpp
+++ transfer.cpp
@@ -1,28 +1,54 @@
 #-----------------------------------------------------------------------
-def findIntersection(n, pt):
-	u = []
-	u.append([20,20]); u.append([7,76]); u.append([57,36]); u.append([107,46])
+def findIntersectionClean(l1, l2, pt):
+	p1 = [0,0]; point = [0,0]
+	# di stands for direction index
+	if abs(l1[1]-l1[1]) > abs(l1[0]-l1[0]):
+		di = 1; dj = 0
+	else: 
+		di = 0; dj = 1
+	pivot = [ l1[di+2], l1[dj+2] ]
+	ppu = ( l1[di+2] - l1[di] ) / ( l1[dj+2] - l1[dj] )# projection per unit
+	line2b = [l2[di], l2[dj], l2[di+2], l2[dj+2]]
+	line2b[di] = line2b[di] + ppu * ( l1[dj+2] - l2[dj] )
+	line2b[di+2] = line2b[di+2] + ppu * ( l1[dj+2] - l2[dj+2] )
+	p1[di] = pivot[di]
+	p1[dj] = line2b[dj] - ( line2b[di] - pivot[di] ) * ( (line2b[dj+2]-line2b[dj])/(line2b[di+2]-line2b[di]) )
+	point[dj] = p1[dj]
+	point[di] = pivot[di] - ppu * ( l1[dj+2] - p1[dj] )
+	return point
+#-----------------------------------------------------------------------
+def findIntersection(l1, l2, pt):
+	# di stands for direction index
+	if abs(l1[1]-l1[1]) > abs(l1[0]-l1[0]):
+		di = 1; dj = 0
+	else: 
+		di = 0; dj = 1
+	print 'we are using the first line to base the projection'
+	print 'and we use direction ', 'X' if di == 0 else 'Y', 'to project our point to'
 
-	seg1x = abs(u[1][0] - u[0][0])
-	seg2x = abs(u[3][0] - u[2][0])
-	minX = seg1x if seg1x < seg2x else seg2x
-	seg1y = abs(u[0][1] - u[1][1])
-	seg2y = abs(u[3][1] - u[2][1])
-	minY = seg1y if seg1y < seg2y else seg2y
-	h = 2 if minX < minY else 0
-	v = 0 if h == 2 else 2
-	if pt[0] == 50 and pt[1] == 50:
-		print 'minX:', minX
-		print 'minY:', minY
-		print 'h:', h
-		print 'v:', v
-
-	projHX = u[h+1][0] - u[h][0]
-	projHY = float( u[h+1][1] - u[h][1] ) / float(projHX)
-
-	#p = ( float(pt[0])-float(u[h]), float(pt[1])-float(u[h+1]), 0 )# delta for pt
-	#ppt = (float(p[0]), float(p[1]) - float(p[0])*float(projHY), 0 )# projection pt
-	#pppt = ( (float(ppt[0]) - float(ppt[1])*float(projVY) ), float(ppt[1]), 0)
+	pivot = [ l1[di+2], l1[dj+2] ]
+	ppu = ( l1[di+2] - l1[di] ) / ( l1[dj+2] - l1[dj] )# projection per unit
+	print 'projection per unit: ', ppu
+	print 'axis localion: ', l1[di+2]
+	line2b = [l2[di], l2[dj], l2[di+2], l2[dj+2]]
+	print l1[dj+2]
+	print line2b
+	line2b[di] = line2b[di] + ppu * ( l1[dj+2] - l2[dj] )
+	line2b[di+2] = line2b[di+2] + ppu * ( l1[dj+2] - l2[dj+2] )
+	print line2b# projected line
+	print 'pivot is: ', pivot[di]
+	p1 = [0,0]
+	p1[di] = pivot[di]
+	p1[dj] = line2b[dj] - ( line2b[di] - pivot[di] ) * ( (line2b[dj+2]-line2b[dj])/(line2b[di+2]-line2b[di]) )
+	print line2b[di]
+	print line2b[dj]
+	print ((line2b[di+2]-line2b[di])/(line2b[dj+2]-line2b[dj]))
+	print 'point on axis: ', p1# projected point
+	point = [0,0]
+	point[dj] = p1[dj]
+	point[di] = pivot[di] - ppu * ( l1[dj+2] - p1[dj] )
+	print 'projected point: ', point# projected point
+	return point
 
 #-----------------------------------------------------------------------
 # find the intersection between two line segments
@@ -39,7 +65,11 @@ imgSize = 128
 txt = Image.new('RGBA', (imgSize,imgSize), (0,0,0,0))#base.size
 d = ImageDraw.Draw(txt)
 lines = []
-lines.append([20,20]); lines.append([7,76]); lines.append([57,36]); lines.append([107,46])
+#lines.append([20.0,20.0,7.0,76.0]); lines.append([57.0,36.0,107.0,46.0])
+#lines.append([10.0,30.0,7.0,76.0]); lines.append([57.0,36.0,107.0,46.0])
+#lines.append([20.0,20.0,7.0,76.0]); lines.append([52.0,45.0,107.0,46.0])
+#lines.append([52.0,45.0,7.0,76.0]); lines.append([20.0,20.0,107.0,46.0])
+lines.append([22.0,22.0,88.0,88.0]); lines.append([22.0,88.0,88.0,22.0])
 aax = [0.25, 0.75, 0.25, 0.75]; aay = [0.25, 0.25, 0.75, 0.75]
 r1 = 100;	g1 = 0;		b1 = 0
 r2 = 0;		g2 = 100;	b2 = 0
@@ -47,29 +77,25 @@ r3 = 0;		g3 = 0;		b3 = 100
 leftEdge = 0; rightEdge = 0
 di = -1
 
-# di stands for direction index
-if abs(lines[0][1]-lines[1][1]) > abs(lines[0][0]-lines[1][0]):
-	di = 1; dj = 0
-else: 
-	di = 0; dj = 1
-B = 0
+B = 0; G = 0
 for i in range(128):
 	for l in range(128):
 		#for aa in range(4):
 			#pt = [float(l)+aax[aa], float(i)+aay[aa]]
-		B = 25
+		B = 25; G = 25
 		d0 = float(lines[0][0]-l)**2 + float(lines[0][1]-i)**2
-		d1 = float(lines[1][0]-l)**2 + float(lines[1][1]-i)**2
-		d2 = float(lines[2][0]-l)**2 + float(lines[2][1]-i)**2
-		d3 = float(lines[3][0]-l)**2 + float(lines[3][1]-i)**2
+		d1 = float(lines[0][2]-l)**2 + float(lines[0][3]-i)**2
+		d2 = float(lines[1][0]-l)**2 + float(lines[1][1]-i)**2
+		d3 = float(lines[1][2]-l)**2 + float(lines[1][3]-i)**2
 		if d0 < 2 or d1 < 2 or d2 < 2 or d3 < 2:
 			B = 255
 		pt = (i,l)
-		result = findIntersection(1, pt)
-		if result:
-			newColor = (25, 25, B)
-		else:
-			newColor = (125, 225, B) 
+		result = findIntersection(lines[0], lines[1], pt)
+
+		d4 = float(result[0]-l)**2 + float(result[1]-i)**2
+		if d4 < 4:
+			G = 255
+		newColor = (125, G, B) 
 		d.point( (l,i), fill = newColor )
 
 txt = txt.resize((512,512))
