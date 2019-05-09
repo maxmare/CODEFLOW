128e0e951b11ff4cbbec7f7e542c2d4141ccf42e
Roberto <roberto@maxmare.com>
2019-05-09 14:31:35 -0700

    Update and rename README.md to README.py

--- README.py
+++ README.py
@@ -1,3 +1,4 @@
+'''
 CODEFLOW
 ========
 
@@ -10,9 +11,10 @@ main()
 {
 	printf("Hello World");
 }
-
+'''
 # reads all source code files in a folder (*.c, cpp, h, hpp, inl),
 # extracts all the definitions and creates a file with them, then creates code-browser links from them
 # to where they are located in the source files
 import sys
 import os
+
