bae176eb6397ce431628197dcdd5e15633387e20
Roberto <roberto@maxmare.com>
2019-05-10 16:34:41 -0700

    Update README.py

--- README.py
+++ README.py
@@ -2,10 +2,14 @@
 CODEFLOW
 ========
 
-Online Memento
-
-/* Hello World program */
 #include<stdio.h>
+
+struct vec3{
+    union A { int a; int b; int c; } typeA;
+    union B { int A; int B; int C; } typeB;
+    float x, y, z;
+    float r, g, b;
+};
 
 main()
 {
@@ -17,4 +21,9 @@ main()
 # to where they are located in the source files
 import sys
 import os
+
+# read the folder pased as argument or pre-determined and gather the files that conform to source file pattern in a list
+
+for file in sourceFiles:
+	lines = file.split()
 
